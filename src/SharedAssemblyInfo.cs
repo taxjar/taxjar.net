d2d8ff83da30c996547092b346ab6697be8baa56
Jake Johnson <jake@fastdivision.com>
2016-09-20 09:30:05 -0700

    Add shared assembly info

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("TaxJar")]
+[assembly: AssemblyCompany("TaxJar")]
+[assembly: AssemblyProduct("TaxJar")]
+[assembly: AssemblyCopyright("Copyright (C) TaxJar 2016")]
+[assembly: AssemblyVersion("1.0.0")]
