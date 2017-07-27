dd06b717556c9344658fb35e1409f1d38a49215b
Jake Johnson <jake@fastdivision.com>
2017-07-27 15:24:04 -0700

    Refactor tax breakdowns, inherit from base class

--- src/Taxjar/Entities/TaxjarBreakdown.cs
+++ src/Taxjar/Entities/TaxjarBreakdown.cs
@@ -2,8 +2,8 @@
 
 namespace Taxjar
 {
-	public class Shipping
-	{
+    public class Breakdown
+    {
 		[JsonProperty("taxable_amount")]
 		public decimal TaxableAmount { get; set; }
 
@@ -16,38 +16,17 @@ namespace Taxjar
 		[JsonProperty("state_taxable_amount")]
 		public decimal StateTaxableAmount { get; set; }
 
-		[JsonProperty("state_sales_tax_rate")]
-		public decimal StateSalesTaxRate { get; set; }
-
-		[JsonProperty("state_amount")]
-		public decimal StateAmount { get; set; }
-
 		[JsonProperty("county_taxable_amount")]
 		public decimal CountyTaxableAmount { get; set; }
 
 		[JsonProperty("county_tax_rate")]
 		public decimal CountyTaxRate { get; set; }
 
-		[JsonProperty("county_amount")]
-		public decimal CountyAmount { get; set; }
-
 		[JsonProperty("city_taxable_amount")]
 		public decimal CityTaxableAmount { get; set; }
 
 		[JsonProperty("city_tax_rate")]
 		public decimal CityTaxRate { get; set; }
-
-		[JsonProperty("city_amount")]
-		public decimal CityAmount { get; set; }
-
-		[JsonProperty("special_taxable_amount")]
-		public decimal SpecialDistrictTaxableAmount { get; set; }
-
-		[JsonProperty("special_tax_rate")]
-		public decimal SpecialDistrictTaxRate { get; set; }
-
-		[JsonProperty("special_district_amount")]
-		public decimal SpecialDistrictAmount { get; set; }
 
 		// International
 		[JsonProperty("country_taxable_amount")]
@@ -86,5 +65,5 @@ namespace Taxjar
 
 		[JsonProperty("qst")]
 		public decimal QST { get; set; }
-	}
+    }
 }
---
+++ src/Taxjar/Entities/TaxjarTaxBreakdownLineItem.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace Taxjar
+{
+	public class TaxBreakdownLineItem : Breakdown
+	{
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("state_sales_tax_rate")]
+		public decimal StateSalesTaxRate { get; set; }
+
+		[JsonProperty("state_amount")]
+		public decimal StateAmount { get; set; }
+
+		[JsonProperty("county_amount")]
+		public decimal CountyAmount { get; set; }
+
+		[JsonProperty("city_amount")]
+		public decimal CityAmount { get; set; }
+
+		[JsonProperty("special_district_taxable_amount")]
+		public decimal SpecialDistrictTaxableAmount { get; set; }
+
+		[JsonProperty("special_tax_rate")]
+		public decimal SpecialTaxRate { get; set; }
+
+		[JsonProperty("special_district_amount")]
+		public decimal SpecialDistrictAmount { get; set; }
+	}
+}
