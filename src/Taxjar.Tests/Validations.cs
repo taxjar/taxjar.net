fcd6086e0c2931cd1c8c9191ef8cc39978adfdfa
Jake Johnson <jake@fastdivision.com>
2018-10-15 13:04:44 -0700

    Add specs for address validation

--- src/Taxjar.Tests/Validations.cs
+++ src/Taxjar.Tests/Validations.cs
@@ -8,7 +8,80 @@ namespace Taxjar.Tests
 	[TestFixture]
 	public class ValidationTests
 	{
-		[Test]
+        [SetUp]
+        public static void Init()
+        {
+            Bootstrap.client = new TaxjarApi(Bootstrap.apiKey, new { apiUrl = "http://localhost:9191" });
+            Bootstrap.server.ResetMappings();
+        }
+
+        [Test]
+        public void when_validating_an_address()
+        {
+            var body = JsonConvert.DeserializeObject<AddressValidationResponse>(TaxjarFixture.GetJSON("addresses.json"));
+
+            Bootstrap.server.Given(
+                Request.Create()
+                    .WithPath("/v2/addresses/validate")
+                    .UsingPost()
+            ).RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithBodyAsJson(body)
+            );
+
+            var addresses = Bootstrap.client.ValidateAddress(new
+            {
+                country = "US",
+                state = "AZ",
+                zip = "85297",
+                city = "Gilbert",
+                street = "3301 South Greenfield Rd"
+            });
+
+            Assert.AreEqual("85297-2176", addresses[0].Zip);
+            Assert.AreEqual("3301 S Greenfield Rd", addresses[0].Street);
+            Assert.AreEqual("AZ", addresses[0].State);
+            Assert.AreEqual("US", addresses[0].Country);
+            Assert.AreEqual("Gilbert", addresses[0].City);
+        }
+
+        [Test]
+        public void when_validating_an_address_with_multiple_matches()
+        {
+            var body = JsonConvert.DeserializeObject<AddressValidationResponse>(TaxjarFixture.GetJSON("addresses_multiple.json"));
+
+            Bootstrap.server.Given(
+                Request.Create()
+                    .WithPath("/v2/addresses/validate")
+                    .UsingPost()
+            ).RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithBodyAsJson(body)
+            );
+
+            var addresses = Bootstrap.client.ValidateAddress(new
+            {
+                state = "AZ",
+                city = "Phoenix",
+                street = "1109 9th"
+            });
+
+            Assert.AreEqual("85007-3646", addresses[0].Zip);
+            Assert.AreEqual("1109 S 9th Ave", addresses[0].Street);
+            Assert.AreEqual("AZ", addresses[0].State);
+            Assert.AreEqual("US", addresses[0].Country);
+            Assert.AreEqual("Phoenix", addresses[0].City);
+
+            Assert.AreEqual("85006-2734", addresses[1].Zip);
+            Assert.AreEqual("1109 N 9th St", addresses[1].Street);
+            Assert.AreEqual("AZ", addresses[1].State);
+            Assert.AreEqual("US", addresses[1].Country);
+            Assert.AreEqual("Phoenix", addresses[1].City);
+        }
+
+        [Test]
 		public void when_validating_a_vat_number()
 		{
             var body = JsonConvert.DeserializeObject<ValidationResponse>(TaxjarFixture.GetJSON("validation.json"));
