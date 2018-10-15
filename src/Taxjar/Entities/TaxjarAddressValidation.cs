501fdd8274b407bb753021c8558e86ca6e69176a
Jake Johnson <jake@fastdivision.com>
2018-10-15 12:58:50 -0700

    Set up `ValidateAddress` client method

---
+++ src/Taxjar/Entities/TaxjarAddressValidation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Taxjar
+{
+    public class AddressValidationResponse
+    {
+        [JsonProperty("addresses")]
+        public List<Address> Addresses { get; set; }
+    }
+
+    public class Address
+    {
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("zip")]
+        public string Zip { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("street")]
+        public string Street { get; set; }
+    }
+}
